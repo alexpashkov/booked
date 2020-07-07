70ce64687cdf8e1a28491bb1f12e96b69ffb54a9
admin <admin@admins-MacBook-Pro.local>
2020-07-08 00:06:29 +0300

    Hello World

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import {ApolloServer} from "apollo-server-express";
+import * as Express from "express"
+import {buildSchema, Query, Resolver} from "type-graphql";
+import "reflect-metadata"
+
+@Resolver()
+class HelloResolver {
+    @Query(() => String)
+    async hello() {
+        return "Hello World"
+    }
+}
+
+async function main() {
+    const schema = await buildSchema({
+        resolvers: [HelloResolver]
+    })
+    const server = new ApolloServer({schema})
+    const app = Express()
+    server.applyMiddleware({app})
+    app.listen(4000, () => console.log("Server started on localhost:4000"))
+}
+
+main()
